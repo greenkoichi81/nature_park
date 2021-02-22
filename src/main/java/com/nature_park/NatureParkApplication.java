5939d62245fc26919f754a1c8e87c38057f8de41
koichi <greenworker.eco@gmail.com>
2021-02-22 23:51:54 +0900

    first

---
+++ src/main/java/com/nature_park/NatureParkApplication.java
@@ -0,0 +1,13 @@
+package com.nature_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatureParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nature_park/NatureParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nature_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatureParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
